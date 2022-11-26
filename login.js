520a09464f58e740670ff072c5393bbfc6fbb812
Grzesiek <grzesiekb@onet.pl>
2022-11-26 22:07:49 +0100

    Added comments, added different login and password

--- login.js
+++ login.js
@@ -1,16 +1,27 @@
+// pobieramy pola formularza za pomocą
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
+// dodajemy event handler do przycisku wysyłającego dane z foromularza
 loginButton.addEventListener("click", (e) => {
+
+    // funkcja ta powoduje ze nie zostanie wykonana standardowa akcja, w tym przypadku nie pozwalamy na odświeżenie strony przed wyświetleniem wyniku działania skryptu
     e.preventDefault();
+
+    // Pobieramy dane z pól formularza
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "" && password === "") {
+    // instrukcja warunkowa sprawdzająca czy dane do logowania wpisane przez użytkownika pokrywają się z tymi ustawionymi przez nas
+    if (username == "test" && password == "test") {
+        // wyświetlenie okienka typu "alert" ze stosowną informacją
         alert("You have successfully logged in.");
+
+        // odświeżenie strony
         location.reload();
     } else {
+        // jeśli użytkownik nie wpisał poprawnych danych logowanie to wyłączamy przezroczystość pola z komunikatem informującym o błędzie
         loginErrorMsg.style.opacity = 1;
     }
 })
